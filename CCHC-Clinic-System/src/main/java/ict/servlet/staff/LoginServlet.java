2376f4ab9b162cfcfbf0e3dffab7f7b43c90c1c0
LOKalex <d4867806@gmail.com>
2026-04-28 17:49:09 +0800

    Move LoginServlet.java to staff package and update package declaration

---
+++ CCHC-Clinic-System/src/main/java/ict/servlet/staff/LoginServlet.java
@@ -0,0 +1,15 @@
+package ict.servlet.staff;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Your doGet code here
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Your doPost code here
+    }
+}
